906203c3dd9fbb5b7ab20173ca0b5574369ba998
kaverikhode <kakhode371223@kkwagh.edu.in>
2026-01-08 11:54:57 +0530

    get id created

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello World"}
 
+
+text_post= {1 : {"title" : "New Post","content" : "cool test post"} }
+@app.get("/posts")
+def get_all_posts():
+    return text_post
+
+@app.get("/posts/{id}")
+def get_id_post(id:int):
+    return text_post.get(id)
 
 
 
